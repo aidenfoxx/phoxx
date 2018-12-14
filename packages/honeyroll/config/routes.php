56e1b3e8e5ff8fcb17a1913a54c90040b61661a5
Aiden Foxx <aiden.foxx.mail@gmail.com>
2018-12-14 20:53:54 +0100

    Removed non-core package

--- packages/honeyroll/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-	'GET' => array(
-		'' => array('Phoxx\Packages\Honeyroll\Controllers\HomeController' => 'index'),
-		'_404_' => array('Phoxx\Packages\Honeyroll\Controllers\PageNotFoundController' => 'index')
-	)
-);
